f6ae01da7b843a8e3aac037300df342b13db4e55
Andrew Devlin and Charles Peden <charles@>
2017-02-13 09:30:16 -0800

    add second test case

--- tests/LeapYearGeneratorTest.php
+++ tests/LeapYearGeneratorTest.php
@@ -15,5 +15,18 @@
             //Assert
             $this->assertEquals(false, $result);
         }
+
+        function test_checkLeapYear_true()
+        {
+            //Arrange
+            $test_LeapYearGenerator = new LeapYearGenerator;
+            $input = 2004;
+
+            //Act
+            $result = $test_LeapYearGenerator->checkLeapYear($input);
+
+            //Assert
+            $this->assertEquals(true, $result);
+        }
     }
 ?>
