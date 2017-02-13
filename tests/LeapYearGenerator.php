6cc1ae26701f127f6eaeb9d45a997e73f0217db9
Andrew Devlin and Charles Peden <andrew@>
2017-02-13 09:19:40 -0800

    create first test case

---
+++ tests/LeapYearGenerator.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "src/LeapYearGenerator.php";
+
+    class LeapYearGeneratorTest extends PHPUnit_Framework_TestCase
+    {
+        function test_checkLeapYear_false()
+        {
+            //Arange
+            $test_LeapYearGenerator = new LeapYearGenerator;
+            $input = 1993;
+
+            //Act
+            $result = $test_LeapYearGenerator->checkLeapYear($input);
+
+            //Assert
+            $this->assertEquals(false, $result);
+        }
+    }
+?>
